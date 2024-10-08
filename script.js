7bc3d1474d737250f9c97a07de96588189f638de
eml <117491033+eml02@users.noreply.github.com>
2024-10-07 23:26:23 -0300

    Add script.js and main functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function suma(sumando1, sumando2) {
+    return sumando1 + sumando2;
+}
+
+function resta(minuendo, sustraendo) {
+    return minuendo - sustraendo;
+}
+
+function multiplicacion(factor1, factor2) {
+    return factor1 * factor2;
+}
+
+function division(dividendo, divisor) {
+    return dividendo / divisor;
+}
